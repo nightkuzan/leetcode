4f993fbdef1edf5000a9fcbe53866cfa04f57e77
nightkuzan <aekkarit12@gmail.com>
2025-06-29 20:10:56 +0700

    reformat

--- 1_twoSum.js
+++ 1_twoSum.js
@@ -3,12 +3,12 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    let map = {}
-    for(let i=0; i< nums.length; i++){
-        if(map.hasOwnProperty(target-nums[i])){
-            return [i,map[target-nums[i]]]
-        }
-        map[nums[i]] = i
+var twoSum = function (nums, target) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (map.hasOwnProperty(target - nums[i])) {
+      return [i, map[target - nums[i]]];
     }
+    map[nums[i]] = i;
+  }
 };
