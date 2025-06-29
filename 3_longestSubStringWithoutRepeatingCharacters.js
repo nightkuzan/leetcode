a78cb88d4ea33d21418ed8da714cc48427b1ad48
nightkuzan <aekkarit12@gmail.com>
2025-06-29 23:54:52 +0700

    longest substring without repeating characters but still repeat

---
+++ 3_longestSubStringWithoutRepeatingCharacters.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string}
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  let max = 0;
+  for (i = 0; i < s.length; i++) {
+    let temp = s.substr(i);
+    let arr = [];
+    for (j = 0; j < temp.length; j++) {
+      if (!arr.includes(temp[j])) {
+        arr.push(temp[j]);
+      } else {
+        break;
+      }
+    }
+    if (arr.length > max) {
+      max = arr.length;
+    }
+  }
+  return max;
+};
